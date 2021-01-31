323b61c4aed7db37ade2a6a09dc65678e7f1f005
Cate Stewart-Gifford <hello@monkehworks.com>
2021-01-31 15:56:07 +0200

    update

--- assets/scripts.js
+++ assets/scripts.js
@@ -3,28 +3,28 @@
 $(document).ready(function(){
 
   // anmsition
-  $(".animsition").animsition({
-    inClass: 'fade-in',
-    outClass: 'fade-out',
-    inDuration: 1500,
-    outDuration: 800,
-    linkElement: '.animsition-link',
-    // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
-    loading: true,
-    loadingParentElement: 'body', //animsition wrapper element
-    loadingClass: 'animsition-loading',
-    loadingInner: '', // e.g '<img src="loading.svg" />'
-    timeout: false,
-    timeoutCountdown: 5000,
-    onLoadEvent: true,
-    browser: [ 'animation-duration', '-webkit-animation-duration'],
-    // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-    // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-    overlay : false,
-    overlayClass : 'animsition-overlay-slide',
-    overlayParentElement : 'body',
-    transition: function(url){ window.location.href = url; }
-  });
+  // $(".animsition").animsition({
+  //   inClass: 'fade-in',
+  //   outClass: 'fade-out',
+  //   inDuration: 1500,
+  //   outDuration: 800,
+  //   linkElement: '.animsition-link',
+  //   // e.g. linkElement: 'a:not([target="_blank"]):not([href^="#"])'
+  //   loading: true,
+  //   loadingParentElement: 'body', //animsition wrapper element
+  //   loadingClass: 'animsition-loading',
+  //   loadingInner: '', // e.g '<img src="loading.svg" />'
+  //   timeout: false,
+  //   timeoutCountdown: 5000,
+  //   onLoadEvent: true,
+  //   browser: [ 'animation-duration', '-webkit-animation-duration'],
+  //   // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+  //   // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+  //   overlay : false,
+  //   overlayClass : 'animsition-overlay-slide',
+  //   overlayParentElement : 'body',
+  //   transition: function(url){ window.location.href = url; }
+  // });
 
 
 
@@ -67,17 +67,17 @@ $(window).load(function() {
 
 
 // fade intro
-(function($){
-  var $window = $(window);
-  $window.scroll(function(){
-    if ($window.scrollTop() >= 160) {
-      $('#intro').fadeOut();
-    }
-    else {
-      $('#intro').fadeIn();
-    }
-  });
-}(jQuery));
+// (function($){
+//   var $window = $(window);
+//   $window.scroll(function(){
+//     if ($window.scrollTop() >= 160) {
+//       $('#intro').fadeOut();
+//     }
+//     else {
+//       $('#intro').fadeIn();
+//     }
+//   });
+// }(jQuery));
 
 
 
