0f923769cf2871e5236136ec047165bfa95456d6
Matt Gifford <matt@distrokid.com>
2021-07-06 15:40:27 +0300

    Adding in formspark form

--- js/scripts.js
+++ js/scripts.js
@@ -1036,52 +1036,20 @@ mr = (function (mr, $, window, document){
 
                 // Hide the error if one was shown
                 formError.fadeOut(200);
-                
+
                 // Create a new loading spinner in the submit button.
                 submitButton.addClass('btn--loading');
 
-                jQuery.ajax({
-                    type: "POST",
-                    url: (formAction !== "" ? formAction : "mail/mail.php"),
-                    data: thisForm.serialize()+"&url="+window.location.href+"&captcha="+captchaUsed,
-                    success: function(response) {
-                        // Swiftmailer always sends back a number representing number of emails sent.
-                        // If this is numeric (not Swift Mailer error text) AND greater than 0 then show success message.
+                var jqxhr = jQuery.post( formAction, thisForm.serialize(), function(){
+                } ).done( function(){
+                    //
+                } ).always( function(){
+                    mr.forms.resetForm(thisForm);
+                    mr.forms.showFormSuccess(formSuccess, formError, 1000, 5000, 500);
+                    mr.forms.captcha.resetWidgets();
+                    submitButton.removeClass('btn--loading');
+                } );
 
-                        submitButton.removeClass('btn--loading');
-
-                        if ($.isNumeric(response)) {
-                            if (parseInt(response,10) > 0) {
-                                // For some browsers, if empty 'successRedirect' is undefined; for others,
-                                // 'successRedirect' is false.  Check for both.
-                                successRedirect = thisForm.attr('data-success-redirect');
-                                if (typeof successRedirect !== typeof undefined && successRedirect !== false && successRedirect !== "") {
-                                    window.location = successRedirect;
-                                }
-
-                                mr.forms.resetForm(thisForm);
-                                mr.forms.showFormSuccess(formSuccess, formError, 1000, 5000, 500);
-                                mr.forms.captcha.resetWidgets();
-                            }
-                        }
-                        // If error text was returned, put the text in the .form-error div and show it.
-                        else {
-                            // Keep the current error text in a data attribute on the form
-                            formError.attr('original-error', formError.text());
-                            // Show the error with the returned error text.
-                            formError.text(response).stop(true).fadeIn(1000);
-                            formSuccess.stop(true).fadeOut(1000);
-                        }
-                    },
-                    error: function(errorObject, errorText, errorHTTP) {
-                        // Keep the current error text in a data attribute on the form
-                        formError.attr('original-error', formError.text());
-                        // Show the error with the returned error text.
-                        formError.text(errorHTTP).stop(true).fadeIn(1000);
-                        formSuccess.stop(true).fadeOut(1000);
-                        submitButton.removeClass('btn--loading');
-                    }
-                });
             }
         }
         return false;
